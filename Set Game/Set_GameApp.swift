79102712baad601378dfb464dd5be843b45e5eea
Eli Manjarrez <manjarrez@apple.com>
2020-10-29 11:51:02 -0700

    Initial Commit

---
+++ Set Game/Set_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  Set_GameApp.swift
+//  Set Game
+//
+//  Created by Eli Manjarrez on 10/29/20.
+//
+
+import SwiftUI
+
+@main
+struct Set_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
